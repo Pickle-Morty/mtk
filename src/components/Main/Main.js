2c52788f23f61fc2449662ad5c1b790d69b2f881
Pickle-Morty <avatarbliz@gmail.com>
2021-06-11 14:13:44 +0600

    remove not used file

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,16 +3,12 @@ import GoodsDelivery from '../GoodsDelivery/GoodsDelivery';
 import HandleQuery from '../HandleQuery/HandleQuery';
 import Header from '../Header/Header';
 import InternationalCompany from '../InternationalCompany/InternationalCompany';
-import Ofice from '../Ofice/Ofice';
 import OurServices from '../OurServices/OurServices';
-import PraiMatch from '../PariMath/PariMatch';
 import Tases from '../Cases/Cases';
-import Products from '../Products/Products';
 import Application from '../Application/Application';
 import Footer from '../Footer/Footer';
 import Plans from '../Plans/Plans'
 import QualityControl from '../QualityControl/QualityControl';
-import WhyWeGotChosen from '../WhyWeGotChosen/WhyWeGotChosen';
 import './Main.css';
 import { Col, Row } from 'react-bootstrap';
 
